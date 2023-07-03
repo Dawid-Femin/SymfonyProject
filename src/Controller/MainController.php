92d473e2b013540f65ee897054c5fc083dfb669a
Dawid Femin <dawid.femin@gmail.com>
2023-07-03 22:02:24 +0200

    Created basic Controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
+$name = 'Dawid';
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('/main', name: 'app_main')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'home')]
+    public function index():object
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainController.php',
-        ]);
+        return new Response(
+            '<h1>Welcome here!</h1>'
+        );
+    }
+    #[Route('/product/{name?}', name: 'product')]
+    public function products(Request $request):object
+    {
+        $var = $request->get('name');
+        return new Response(
+            '<h1>Your product is ' . $var . '!</h1>'
+        );
     }
 }
